d00f9c7f5700ca5e55a9e335de4a4e170dc8e811
Lucha3sr <2278@holbertonschool.com>
2021-02-12 17:08:08 +0000

    AirBnB clone - The console

--- models/base_models.py
+++ models/base_models.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""This class defines all common attributes/methods for other classes"""
+
+import uuid
+
+class BaseModel:
+
+    """class containing Public instance attributes"""
+
+    def __init__(self):
+
+        """generate unique id converted to str"""
+        format_time = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def to_dict(self, ):
+
+        """create a dictionary"""
+        
+
+    def __str__(self):
+
+        """str repr of class"""
+        return "[BaseModel] ({}) <{}>" .format(self.id, self.__dict__)
