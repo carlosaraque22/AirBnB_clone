f8ef8a1c516c3f63f7cee0b9401fbeb1b078e968
Carlos Araque <cararqueu@gmail.com>
2021-02-12 17:17:47 +0000

    update basemodels

--- models/base_models.py
+++ models/base_models.py
@@ -3,18 +3,24 @@
 """This class defines all common attributes/methods for other classes"""
 
 import uuid
+from datetime import datetime
 
 class BaseModel:
 
     """class containing Public instance attributes"""
 
-    def __init__(self):
+    def __init__(self, **argdict):
 
         """generate unique id converted to str"""
-        format_time = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if argdict:
+            for key, value in argdict.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
 
     def to_dict(self, ):
 
