eb3e59ad26bbc4ee8ad12203605d1acaba570ba4
Lucha3sr <2278@holbertonschool.com>
2021-02-12 20:01:05 +0000

    AirBnB clone - The console

--- models/base_models.py
+++ models/base_models.py
@@ -5,29 +5,40 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
 
     """class containing Public instance attributes"""
 
-    def __init__(self, **argdict):
+    def __init__(self, *args, **kwargs):
 
         """generate unique id converted to str and format datatime"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if argdict:
-            for key, value in argdict.items():
+        if kwargs:
+            for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = value
 
-
     def __str__(self):
-        """str repr of class"""
-        return "[{}] ({}) {}".format(self.__class__.name__,
+        """string representation of class"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id,
                                      self.__dict__)
 
 
-    
+    def save(self):
+        """save class data into a file to use it later"""
+        self.updated_at = datetime.now()
+        models.file_storage.save()
+
+    def to_dict(self):
+        """returns a distionary containing all keys/values of __dict__"""
+        new_dict = self.__dict__.copy()
+        new_dict[__class__] = self.__class__.__name__
+        new_dict[created_at] = self.created_at.isoformat()
+        new_dict[updated_at] = self.updated_at.isoformat()
