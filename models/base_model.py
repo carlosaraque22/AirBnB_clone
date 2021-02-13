5e87002d8d15643357b5fdc7019ffd8205e3ec1a
Lucha3sr <2278@holbertonschool.com>
2021-02-13 17:17:28 +0000

    AirBnB clone - The console

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
 
@@ -26,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """string representation of class"""
