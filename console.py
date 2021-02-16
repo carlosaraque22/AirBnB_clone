cf925dcf115db2215419fed900cdaa8d77770488
Carlos Araque <cararqueu@gmail.com>
2021-02-16 00:32:38 +0000

    Task 7 in process by Seba, Lu, Pedro, Yo

--- console.py
+++ console.py
@@ -48,6 +48,45 @@ class HBNBCommand(cmd.Cmd):
             return False
         print(new_object.id)
         new_object.save()
-          
+
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+        command = arg.split()
+        models.storage.reload()
+        if (len(command) == 0):
+            print("** class name missing **")
+            return False
+        if command[0] in found_classes:
+            if (len(command) > 1):
+                new_object = command[0] + '.' + command[1]
+                if new_object in models.storage.all():
+                    print(models.storage.all()[new_object])
+                else:
+                    print("** no instance found **")
+            elif (len(command) == 1):
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        command = arg.split()
+        models.storage.reload()
+        if (len(command) == 0):
+            print("** class name missing **")
+            return False
+        if command[0] in found_classes:
+            if (len(command) > 1):
+                new_object = command[0] + '.' + command[1]
+                if new_object in models.storage.all():
+                    models.storage.all().pop(new_object)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            elif (len(command) == 1):
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
