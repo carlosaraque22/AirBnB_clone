0ae7131ad7dc11ed37b1b9ca2ce82d452407a35f
Carlos Araque <cararqueu@gmail.com>
2021-02-17 22:46:03 +0000

    fgix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,7 +9,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import unittest
 
 
 class Testuser(unittest.TestCase):
