b3ea4eb223343f999a1e3b645e39caf36666a966
Carlos Araque <cararqueu@gmail.com>
2021-02-17 22:48:50 +0000

    fix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,19 +9,17 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-
+import unittest
 
 class Testuser(unittest.TestCase):
     """tests for the User class"""
 
     def test_class_name(self):
         """Tests if class is named correctly"""
-
         user_example = User()
         self.assertEqual(user_example.__class__.__name__, "User")
 
     def test_inheritance(self):
         """Tests if Class inherits from BaseModel"""
-
         user_example = User()
         self.assertTrue(issubclass(user_example.__class__, BaseModel))
