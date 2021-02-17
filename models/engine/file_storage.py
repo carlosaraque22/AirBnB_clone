9235c31a65f8c3d3d649079bacd0bf9f02c4c18e
Carlos Araque <cararqueu@gmail.com>
2021-02-17 22:59:44 +0000

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,11 +45,10 @@ class FileStorage:
         """deserializes the JSON file to __objects, only if the JSON file"""
         """__file_path exists ; otherwise, do nothing. If the file doesnt"""
         """exist, no exception should be raised"""
-        if path.exists(self.__file_path):
-            try:
-                with open(self.__file_path, "r") as file:
-                    __objects = json.load(file)
-                for key, value in __objects.items():
-                    self.__objects[key] = eval(value['__class__'])(**value)
-            except:
-                pass
+        try:
+            with open(self.__file_path, "r") as file:
+               new_objects = json.load(file)
+            for key, value in new_objects.items():
+                self.__objects[key] = eval(value['__class__'])(**value)
+        except:
+            pass
