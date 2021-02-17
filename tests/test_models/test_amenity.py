00d54caa31d983ea0b63ace36c0001113f6db698
Lucha3sr <2278@holbertonschool.com>
2021-02-17 19:25:19 +0000

    AirBnB clone - The console

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Test class Amenity"""
+
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import unittest
+
+
+class Testamenity(unittest.TestCase):
+    """test for amenity class"""
+
+    def test_class(self):
+        """ tests if the class is named correctly"""
+
+        amenity_example = Amenity()
+        self.assertEqual(amenity_example.__class__.__name__, "Amenity")
+
+    def test_inheritance(self):
+        """Tests if class inherits from BaseModel"""
+
+        amenity_example = Amenity()
+        self.assertTrue(issubclass(amenity_example.__class__, BaseModel))
