e2d3d73e3e567fef671782954078476f628f521e
Lucha3sr <2278@holbertonschool.com>
2021-02-17 17:45:38 +0000

    AirBnB clone - The console

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-"""Defines unittests for models/engine/file_storage.py.
-Unittest classes:
-    TestFileStorage_instantiation
-    TestFileStorage_methods
-"""
-import os
-import json
+
+""" Module to test class File Storage"""
+
 import unittest
-from datetime import datetime
+import json
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from os import path
 from models.user import User
 from models.state import State
-from models.place import Place
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage_instantiation(unittest.TestCase):
-        """Unittests for testing instantiation of the FileStorage class."""
-
-        def test_FileStorage_instantiation_no_args(self):
-            self.assertEqual(type(FileStorage()), FileStorage)
-
-        def test_FileStorage_instantiation_with_arg(self):
-            with self.assertRaises(TypeError):
-                FileStorage(None)
-
-        def test_FileStorage_file_path_is_private_str(self):
-            self.assertEqual(str, type(FileStorage._FileStorage__file_path))
-
-        def testFileStorage_objects_is_private_dict(self):
-            self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-        def test_storage_initializes(self):
-            self.assertEqual(type(models.storage), FileStorage)
-
-        if __name__ == "__main__":
-            unittest.main()
+class TestFileStorage(unittest.TestCase):
+    """test File Storage"""
+    def test_all_methods(self):
+        """test all methods"""
+        #  Dict_storage = FileStorage()
+        # new_dict_storage = Dict_storage.all()
+        # self.assertEqual(type(new_dict_storage), dict)
