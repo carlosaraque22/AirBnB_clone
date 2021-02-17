899b5eeeabe544b30c0843b0d792cf86e6cfe645
Carlos Araque <cararqueu@gmail.com>
2021-02-17 23:34:20 +0000

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,7 +48,7 @@ class FileStorage:
         if path.exists(self.__file_path)
         try:
             with open(self.__file_path, "r") as file:
-               new_objects = json.loads(file)
+               new_objects = json.load(file)
             for key, value in new_objects.items():
                 self.__objects[key] = eval(value['__class__'])(**value)
         except:
