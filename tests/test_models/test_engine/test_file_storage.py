0bbc9a70110c5b4b679ed0a1808337b45c2484a0
Carlos Araque <cararqueu@gmail.com>
2021-02-17 22:39:42 +0000

    fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,17 +2,17 @@
 
 """ Module to test class File Storage"""
 
-#import unittest
-#import json
-#from models.base_model import BaseModel
-#from os import path
-#from models.user import User
-#from models.state import State
-#from models.city import City
-#from models.amenity import Amenity
-#from models.place import Place
-#from models.review import Review
-#from models.engine.file_storage import FileStorage
+import json
+import unittest
+from models.base_model import BaseModel
+from os import path
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
@@ -21,6 +21,6 @@ class TestFileStorage(unittest.TestCase):
 
     def test_all_methods(self):
         """test all methods"""
-        #  Dict_storage = FileStorage()
-        # new_dict_storage = Dict_storage.all()
-        # self.assertEqual(type(new_dict_storage), dict)
+        Dict_storage = FileStorage()
+        new_dict_storage = Dict_storage.all()
+        self.assertEqual(type(new_dict_storage), dict)
