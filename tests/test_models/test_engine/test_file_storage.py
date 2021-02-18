717a94234901d8de9e3e8ad507bcf2f6cc1efb14
Carlos Araque <cararqueu@gmail.com>
2021-02-18 00:19:27 +0000

    fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,26 +1,31 @@
-#!/usr/bin/python3
-
-""" Module to test class File Storage"""
-
-import json
-import unittest
-from models.base_model import BaseModel
-from os import path
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+""" Module tests/test_models/test_engine/test_file_storage"""
+import models
 from models.engine.file_storage import FileStorage
+import os
+import unittest
 
 
-class TestFileStorage(unittest.TestCase):
+class TestFile_Storage(unittest.TestCase):
+    """ class TestFile_Storage """
 
-    """test File Storage"""
+    def test_docstring(self):
+        """ function test_docstring """
+        msj = "Module doesnt have docstring"
+        obj = models.engine.file_storage.__doc__
+        self.assertIsNotNone(obj, msj)
+        msj = "Classes doesnt have docstring"
+        self.assertIsNotNone(obj, msj)
 
-    def test_all_methods(self):
-        """test all methods"""
-        Dict_storage = FileStorage()
-        new_dict_storage = Dict_storage.all()
-        self.assertEqual(type(new_dict_storage), dict)
+    def test_executable_file(self):
+        """ function test_executable_file """
+        is_read_true = os.access("models/engine/file_storage.py", os.R_OK)
+        self.assertTrue(is_read_true)
+        is_write_true = os.access("models/engine/file_storage.py", os.W_OK)
+        self.assertTrue(is_write_true)
+        is_exec_true = os.access("models/engine/file_storage.py", os.X_OK)
+        self.assertTrue(is_exec_true)
+
+    def test_is_an_instance(self):
+        """ function test_is_an_instance """
+        my_model = FileStorage()
+        self.assertIsInstance(my_model, FileStorage)
